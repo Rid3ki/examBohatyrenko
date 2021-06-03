9682fbeddac949d984885a6ba88632c216309802
Rid3ki <78948504+Rid3ki@users.noreply.github.com>
2021-06-03 09:47:59 +0300

    Add files via upload

---
+++ func1.m
@@ -0,0 +1,20 @@
+function y = func1(n){
+  
+  var y = 0;
+  
+  for i = 1:n
+    
+  var jCount = 1;
+    for j = 1:5
+      
+      jCount = jCount .* sin(1/j);
+      
+    endfor
+    
+    iCount = iCount + jCount;
+    
+  endfor
+  
+  
+
+  }
